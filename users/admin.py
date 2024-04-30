89aa0346b9043e99bbdeedaf3fa9137b1fc9d492
BurmAnton <a.burmatsky@gmail.com>
2024-04-30 17:52:14 +0400

    register User, Group and Permission models in users_app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group as DefaultGroup
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+
+from .models import User, Group, Permission
+
 
 # Register your models here.
+admin.site.unregister(DefaultGroup)
+
+@admin.register(Group)
+class GroupAdmin(GroupAdmin):
+    pass
--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
 
 # Create your models here.
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    username = None
+    middle_name = models.CharField("Отчество", max_length=30, blank=True, null=True)
+    email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField("Номер телефона", max_length=20, blank=True, null=True)
+    
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+class Group(Group):
+    
+    class Meta:
+        verbose_name = "Группа пользователей"
+        verbose_name_plural = "Группы пользователей"
+
+
+class Permission(Permission):
+    
+    class Meta:
+        verbose_name = "Разрешение"
+        verbose_name_plural = "Разрешения"
